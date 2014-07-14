8c182640fa426d079ef904b3090aee7427e32144
irdb <irdb@users.noreply.github.com>
2014-07-14 19:08:27 +0430

    Rewrite + bug fix: could not detect year numbers.

--- bibtex.py
+++ bibtex.py
@@ -3,9 +3,15 @@
 
 """This module is used for parsing BibTeX entries.
 
-Known issues:
+The goal of this code is to parse BibTeX entries from a number of known sites
+with known BibTeX formats; therefore many other BiBteX entries may parse
+incorrectly or incompletely as TeX system is very complex and this module is
+not intended to parse TeX.
+
+Some of the known issues:
     * Currently it does not detect special symbols and many TeX escape
         sequences (more information: http://www.bibtex.org/SpecialSymbols/)
+    * String concatinatins are not recognized. ("str1" # "str2")
 """
 
 import re
@@ -15,99 +21,70 @@ import commons
 
 
 
-def parse(bibtex_text):
-    """Parse bibtex_text data and return a dictionary of information."""
-    # replacing common latex special symbols commonds
-    bibtex_text = bibtex_text.replace(r'{\textregistered}', '®')
-    bibtex_text = bibtex_text.replace(r'\%', '%')
-    bibtex_text = bibtex_text.replace(r'\$', '$')
-    bibtex_text = bibtex_text.replace(r'\{', '{')
-    bibtex_text = bibtex_text.replace(r'\}', '}')
-    bibtex_text = bibtex_text.replace(r'\#', '#')
-    bibtex_text = bibtex_text.replace(r'\&', '&')
-    bibtex_text = bibtex_text.replace(r'{\={a}}', 'ā')
-    bibtex_text = bibtex_text.replace(r'{\v{c}}', 'č')
-    bibtex_text = bibtex_text.replace(r'{\={e}}', 'ē')
-    bibtex_text = bibtex_text.replace(r'{\v{g}}', 'ģ')
-    bibtex_text = bibtex_text.replace(r'{\={\i}}', 'ī')
-    bibtex_text = bibtex_text.replace(r'{\c{k}}', 'ķ')
-    bibtex_text = bibtex_text.replace(r'{\c{l}}', 'ļ')
-    bibtex_text = bibtex_text.replace(r'{\c{n}}', 'ņ')
-    bibtex_text = bibtex_text.replace(r'{\v{s}}', 'š')
-    bibtex_text = bibtex_text.replace(r'{\={u}}', 'ū')
-    bibtex_text = bibtex_text.replace(r'{\v{z}}', 'ž')
-    bibtex_text = bibtex_text.replace(r'{\={A}}', 'Ā')
-    bibtex_text = bibtex_text.replace(r'{\v{C}}', 'Č')
-    bibtex_text = bibtex_text.replace(r'{\={E}}', 'Ē')
-    bibtex_text = bibtex_text.replace(r'{\c{G}}', 'Ģ')
-    bibtex_text = bibtex_text.replace(r'{\={I}}', 'Ī')
-    bibtex_text = bibtex_text.replace(r'{\c{K}}', 'Ķ')
-    bibtex_text = bibtex_text.replace(r'{\c{L}}', 'Ļ')
-    bibtex_text = bibtex_text.replace(r'{\c{N}}', 'Ņ')
-    bibtex_text = bibtex_text.replace(r'{\v{S}}', 'Š')
-    bibtex_text = bibtex_text.replace(r'{\={U}}', 'Ū')
-    bibtex_text = bibtex_text.replace(r'{\v{Z}}', 'Ž')
-    d = {}
+def parse(bibtex):
+    """Parse bibtex string and return a dictionary of information."""
+    bibtex = replace_specials(bibtex)
+    d = search_for_tag(bibtex)
     # type: (book, journal, . . . )
-    m = re.search('@(.*?)\s*\{', bibtex_text, re.I)
+    m = re.search('@(.*?)\s*\{', bibtex, re.I)
     if m:
         d['type'] = m.group(1).strip().lower()
     # author
-    m = re.search('author\s*=\s*\{\s*(.*?)\\s*}', bibtex_text, re.I)
-    if m:
+    if 'author' in d:
         d['authors'] = []
-        authors = m.group(1).split(' and ')
-        for author in authors:
+        for author in d['author'].split(' and '):
             name = commons.Name(author)
             d['authors'].append(name)
-
-    m = re.search('title\s*=\s*\{\s*(.*?)\s*\}', bibtex_text, re.I)
-    if m:
-        d['title'] = m.group(1).strip()
-    m = re.search('publisher\s*=\s*\{\s*(.*?)\s*\}', bibtex_text, re.I)
-    if m:
-        d['publisher'] = m.group(1).strip()
-    m = re.search('journal\s*=\s*\{\s*(.*?)\s*\}', bibtex_text, re.I)
-    if m:
-        d['journal'] = m.group(1).strip()
-    m = re.search('number\s*=\s*\{\s*(.*?)\s*\}', bibtex_text, re.I)
-    if m:
-        d['issue'] = m.group(1).strip()
-    m = re.search('series\s*=\s*\{\s*(.*?)\s*\}', bibtex_text, re.I)
-    if m:
-        d['series'] = m.group(1).strip()
-    m = re.search('volume\s*=\s*\{\s*(.*?)\s*\}', bibtex_text, re.I)
-    if m:
-        d['volume'] = m.group(1).strip()
-    m = re.search('year\s*=\s*\{\s*(.*?)\s*\}', bibtex_text, re.I)
-    if m:
-        d['year'] = m.group(1).strip()
-    m = re.search('month\s*=\s*\{\s*(.*?)\s*\}', bibtex_text, re.I)
-    if m:
-        d['month'] = m.group(1).strip()
-    m = re.search('isbn\s*=\s*\{\s*(.*?)\s*\}', bibtex_text, re.I)
-    if m:
-        d['isbn'] = m.group(1).strip()
-    m = re.search('lccn\s*=\s*\{\s*(.*?)\s*\}', bibtex_text, re.I)
-    if m:
-        d['lccn'] = m.group(1).strip()
-    m = re.search('pages\s*=\s*\{\s*(.*?)\s*\}', bibtex_text, re.I)
-    if m:
-        d['pages'] = m.group(1).strip()
+        del d['author']
+    if 'number' in d:
+        d['issue'] = d['number']
+        del d['number']
+    if 'pages' in d:
         d['pages'] = d['pages'].replace('--', '–')
         d['pages'] = d['pages'].replace('-', '–')
         if '–' in d['pages']:
             d['startpage'], d['endpage'] = d['pages'].split('–')
-    m = re.search('url\s*=\s*\{\s*(.*?)\s*\}', bibtex_text, re.I)
-    if m:
-        d['url'] = m.group(1).strip()
-    m = re.search('doi\s*=\s*\{\s*(.*?)\s*\}', bibtex_text, re.I)
-    if m:
-        d['doi'] = m.group(1).strip()
-    m = re.search('address\s*=\s*\{\s*(.*?)\s*\}', bibtex_text, re.I)
-    if m:
-        d['address'] = m.group(1).strip()
-    m = re.search('language\s*=\s*\{\s*(.*?)\s*\}', bibtex_text, re.I)
-    if m:
-        d['language'] = m.group(1).strip()
     return d
+
+
+def search_for_tag(bibtex):
+    """Find all tags in the bibtex and return result as a dictionary."""
+    d = {}
+    fs = re.findall('(\w+)\s*=\s*(?:[{"]\s*(.*?)\s*["}]|(\d+))', bibtex)
+    for f in fs:
+        d[f[0].lower()] = f[1] if f[1] else f[2]
+    return d
+
+def replace_specials(bibtex):
+    """Replace common TeX special symbol commonds with their unicode value."""
+    bibtex = bibtex.replace(r'{\textregistered}', '®')
+    bibtex = bibtex.replace(r'\%', '%')
+    bibtex = bibtex.replace(r'\$', '$')
+    bibtex = bibtex.replace(r'\{', '{')
+    bibtex = bibtex.replace(r'\}', '}')
+    bibtex = bibtex.replace(r'\#', '#')
+    bibtex = bibtex.replace(r'\&', '&')
+    bibtex = bibtex.replace(r'{\={a}}', 'ā')
+    bibtex = bibtex.replace(r'{\v{c}}', 'č')
+    bibtex = bibtex.replace(r'{\={e}}', 'ē')
+    bibtex = bibtex.replace(r'{\v{g}}', 'ģ')
+    bibtex = bibtex.replace(r'{\={\i}}', 'ī')
+    bibtex = bibtex.replace(r'{\c{k}}', 'ķ')
+    bibtex = bibtex.replace(r'{\c{l}}', 'ļ')
+    bibtex = bibtex.replace(r'{\c{n}}', 'ņ')
+    bibtex = bibtex.replace(r'{\v{s}}', 'š')
+    bibtex = bibtex.replace(r'{\={u}}', 'ū')
+    bibtex = bibtex.replace(r'{\v{z}}', 'ž')
+    bibtex = bibtex.replace(r'{\={A}}', 'Ā')
+    bibtex = bibtex.replace(r'{\v{C}}', 'Č')
+    bibtex = bibtex.replace(r'{\={E}}', 'Ē')
+    bibtex = bibtex.replace(r'{\c{G}}', 'Ģ')
+    bibtex = bibtex.replace(r'{\={I}}', 'Ī')
+    bibtex = bibtex.replace(r'{\c{K}}', 'Ķ')
+    bibtex = bibtex.replace(r'{\c{L}}', 'Ļ')
+    bibtex = bibtex.replace(r'{\c{N}}', 'Ņ')
+    bibtex = bibtex.replace(r'{\v{S}}', 'Š')
+    bibtex = bibtex.replace(r'{\={U}}', 'Ū')
+    bibtex = bibtex.replace(r'{\v{Z}}', 'Ž')
+    return bibtex
+    
