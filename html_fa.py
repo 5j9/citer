220faf5b443817809608a0608f3a9d72f26d86d7
irdb <irdb@users.noreply.github.com>
2014-07-23 11:15:50 +0430

    update

--- html_fa.py
+++ html_fa.py
@@ -1,84 +1,153 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
-"""HTML skeleton of the application and its predefined resposes."""
+"""HTML skeleton of the application and its predefined responses."""
 
 
-class ResposeObj():
+from string import Template
+from datetime import date
 
-    """Create the response object used in the main application."""
+class Respose:
 
-    def __init__(self, ref, cite, error):
-        self.sfnt = ref
-        self.ctnt = cite
+    """Create the responce object used by the main application."""
+
+    def __init__(self, sfnt, ctnt='', reft='', error='100'):
+        self.sfnt = sfnt
+        self.ctnt = ctnt
+        self.reft = reft
         self.error = error
 
 
-skeleton = """<!DOCTYPE html>
+def response_to_template(response):
+    """Insert the response into the template and return response_body."""
+    return template % (response.sfnt,
+                       response.ctnt,
+                       response.reft,
+                       response.error)
+
+
+template = Template("""<!DOCTYPE html>
 <html dir="rtl">
-<head>
- <title>یادکرد</title>
- <style type="text/css">
-  select, textarea, input {
-   background-color: rgb(255, 255, 255);
-   border: 1px solid rgb(204, 204, 204);
-   padding: 2px 2px;
-   margin-bottom: 10px;
-   font-size: 14px;
-   line-height: 16px;
-   color: rgb(85, 85, 85);
-   vertical-align: middle;
-   border-radius: 5px 5px 5px 5px;
-   }
-  body {
-  font-family: tahoma;
-  font-size:0.8em
-  }
-  </style>
-</head>
-<body>
- <form method="get" action="yadkard.fcgi">
-  <p>
-   نشانی وب:<br><input type="text" size="100" name="url">
-  <input type="submit" value="یادکرد">
-  </p>
- </form>
-  <p>
-   پانویس کوتاه‌شده و یادکرد:<br>
-   <textarea rows="15" cols="80" readonly>%s\n\n%s</textarea>
-  </p>
-  <p>
-   احتمال خطا در تشخیص زبان: %s ٪
-  </p>
-</body>
-</html>"""
+    <head>
+        <title>یادفا (ابزار ساخت یادکرد برای ویکی‌پدیای فارسی)</title>
+        <style type="text/css">
 
-default_response = (
-    'این ابزار برای تولید یادکرد مناسب ویکی‌پدیای فارسی، برابر شیوه‌نامهٔ شیکا\
-گو، کاربرد دارد.',
-    'هم‌اکنون از وب‌گاه‌های زیر پشتیبانی می‌شود:\n\
-* http://books.google.com (گوگل بوکس)\n\
-* http://www.noormags.com (نورمگز)\n\
-* http://www.noorlib.ir (کتابخانه دیجیتال نور)\n\
-* http://www.adinebook.com (آدینه‌بوک)\n\
-* http://dx.doi.org (یا متنی که شامل «شناسانۀ برنمود رقمی» (doi) باشد)\n\
-* شابک (ISBN) برای بیشتر کتاب‌ها (ایرانی و خارجی)\n\n\
-در صورت بروز مشکل یا درست عمل نکردن ابزار می‌توانید با من (دالبا) تماس \
-بگیرید. امکان گسترش ابزار برای کتابخانه‌های دیجیتالی که خروجی Bibtex یا \
-RefMan ‏(RIS) تولید می‌کنند وجود دارد.',
-    '؟؟')
+            textarea, input {
+                transition: background-color 5s ease-in;
+                background-color: rgb(255, 255, 255);
+                border: 1px solid rgb(204, 204, 204);
+                padding: 2px 2px;
+                margin-bottom: 10px;
+                font-size: 14px;
+                line-height: 16px;
+                color: rgb(85, 85, 85);
+                vertical-align: middle;
+                border-radius: 5px 5px 5px 5px;
+                }
+            textarea{
+                display:block;
+                margin-left: auto;
+                margin-right: auto;
+                width:100%;
+                word-break: break-all;
+                }
+            body {
+                font-family: tahoma;
+                font-size:0.8em
+                }
+            input[type=text]{
+                width:50%;
+                }
+            input[type=submit]{
+                float:left;
+                }
+            #info{
+                font-size:90%;
+                color:#666666;
+                }
+            input[type=submit]:hover{
+                transition: background-color 1s ease-in;
+                background-color:#33CC33;
+                }
+        </style>
+    </head>
+    <body>
+        <div style="margin-left:auto; margin-right:auto; width:62%;">
+            <form method="get" action="yadkard.fcgi">
+                <p>
+                    نشانی وب/شابک/شناسهٔ برنمود رقمی:<br><input type="text" name="user_input">
+                    <input type="submit" value="ثبت درخواست">
+                </p>
+            </form>
+            <p>
+                <a href="https://en.wikipedia.org/wiki/Help:Shortened_footnotes">پانویس کوتاه‌شده</a> و یادکرد:<br>
+                <textarea rows="6" readonly>$s\n\n$s</textarea>
+                <a href="https://en.wikipedia.org/wiki/Wikipedia:NAMEDREFS#WP:NAMEDREFS">برچسب ارجاع درون‌خطی</a>:<br>
+                <textarea rows="4" readonly>$s</textarea>
+            </p>
+            <p>
+                <!-- There may be error in language detection. $s % -->
+            </p>
+            <div id="info">
+                <p>
+                        با استفاده از این ابزار می‌توانید برچسب ارجاع (تگ <ref>) و یا پانویس کوتاه‌دشده و یادکرد مورد استفاده در ویکی‌پدیای فارسی برای ارجاع به منابع را بسازید. ابزار موارد زیر را به عنوان ورودی می‌پذیرد:</p>
+                <p>
+                        نشانی وب <a href="http://books.google.com/">کتاب‌های گوگل</a>، <a href="https://en.wikipedia.org/wiki/Digital_object_identifier">شناسانۀ برنمود رقمی</a>، یا <a href="https://en.wikipedia.org/wiki/International_Standard_Book_Number">شابک</a> (حتی برای بسیاری از کتاب‌های فارسی).</p>
+                <p>
+                        افزون بر این‌ها می‌توانید نشانی وب خبرهای بسیاری از خبرگزاری‌های مهم را نیز در آن آزمایش کنید. توجه داشته باشید که همواره احتمال خطا در خروجی ابزار وجود دارد و نیازمند بازبینی خواهد بود. <b>با مسئولیت خودتان از آن استفاده کنید.<b></p>
+            </div>
+        </div>
+        <script>
+            function setCookie(cname, cvalue, exdays) {
+                var d = new Date();
+                d.setTime(d.getTime() + (exdays*24*60*60*1000));
+                var expires = "expires="+d.toGMTString();
+                document.cookie = cname + "=" + cvalue + "; " + expires;
+            }
 
-undefined_url_response = ('نشانی واردشده برای این ابزار تعریف نشده‌است.',
-                          'اگر کتابخانهٔ دیجیتالی می‌شناسید که خروجی \
-bibtex یا RIS تولید می‌کند، لطفاً موضوع را با توسعه‌دهنده ٔ ابزار در میان بگذارید\
- تا در صورت امکان به ابزار افزوده شود.',
-                          '۱۰۰')
+            function getCookie(cname) {
+                var name = cname + "=";
+                var ca = document.cookie.split(';');
+                for(var i=0; i<ca.length; i++) {
+                    var c = ca[i].trim();
+                    if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+                }
+                return "";
+            }
 
-httperror_response = ('خطای اچ‌تی‌تی‌پی در دریافت اطلاعات.',
-                      'اطلاعات قابل دسترس نبودند.',
-                      '۱۰۰')
+            function checkCookie() {
+                var datefmt = getCookie('datefmt');
+                if (datefmt != '') {
+                  document.getElementById(datefmt).checked = true;
+                }
+            }
+            checkCookie()
+        </script>
+    </body>
+</html>""")
 
-other_exception_response = (
-    'خطای پیش‌بینی‌نشده‌ای رخ داد.',
-    'لطفاً مطمئن شوید که نشانی وب را درست وارد کرده‌اید.',
-    '۱۰۰')
+# Predefined responses
+default_response = Respose('یادکرد ساخته‌شده اینجا نمایان خواهد شد...',
+                           '',
+                           '',
+                           '??')
+
+undefined_url_response = Respose('ورودی شناخته نشد.',
+                                 'خطا در سیاههٔ خطاها ثبت شد.')
+
+httperror_response = Respose('خطای اچ‌تی‌تی‌پی:',
+                             'یک یا چند مورد از منابع اینترنتی مورد ' +
+                             'نیاز برای ساخت این یادکرد در این لحظه ' +
+                             'در دسترس نیستند. (و یا ورودی نامعتبر است)')
+
+other_exception_response = Respose('خطای ناشناخته‌ای رخ داد..',
+                                   'اطلاعات خطا در سیاهه ثبت شد.')
+
+today = date.today()
+template = template.safe_substitute({'Ymd': today.strftime('%Y-%m-%d'),
+                                     'BdY': today.strftime('%B %d, %Y'),
+                                     'bdY': today.strftime('%b %d, %Y'),
+                                     'dBY': today.strftime('%d %B %Y'),
+                                     'dbY': today.strftime('%d %b %Y'),
+                                     }).replace('%', '%%').replace('$', '%')
+
--- tests/test_fa.py
+++ tests/test_fa.py
@@ -4,6 +4,7 @@
 import unittest
 import sys
 
+import dummy_requests
 sys.path.append('..')
 import adinebook
 import googlebooks
@@ -18,49 +19,49 @@ class AdinebookTest(unittest.TestCase):
     def test_ab1(self):
         """authors = 1, translators = 2, otheo = 1, isbn13"""
         i = 'http://www.adinebook.com/gp/product/9648165814/ref=sr_1_1000_42/905-6618179-9188955'
-        o = adinebook.AdineBook(i)
+        o = adinebook.Response(i)
         e = '* {{یادکرد کتاب|نام خانوادگی=لانسکی|نام=ویکی|ترجمه=فیروزه دالکی و مژگان امیرفروغی|دیگران= کی وایت (تصويرگر)|کتاب=101 راه برای اینکه پدر بهتری باشید|ناشر=پیک ادبیات|سال=1386|ماه=شهریور|شابک=978-964-8165-81-4|زبان=fa|تاریخ بازبینی='
         self.assertIn(e, o.ctnt)
 
     def test_ab2(self):
         """authors = 3, translators = 2, otheo = 0, isbn13"""
         i = 'http://www.adinebook.com/gp/product/9642823352/ref=sr_1_1000_41/905-6618179-9188955'
-        o = adinebook.AdineBook(i)
+        o = adinebook.Response(i)
         e = '* {{یادکرد کتاب|نام خانوادگی=کرسول|نام=جان|نام خانوادگی۲=کلارک|نام۲=ویکی پلانو|ترجمه=محسن نیازی و عباس زارعی|کتاب=روش های تحقیق تلفیقی|ناشر=علم و دانش|سال=1387|ماه=خرداد|شابک=978-964-2823-35-2|زبان=fa|تاریخ بازبینی='
         self.assertIn(e, o.ctnt)
 
     def test_ab3(self):
         """authors = 2, translators = 0, otheo = 4, isbn13"""
         i = 'http://www.adinebook.com/gp/product/6005883435'
-        o = adinebook.AdineBook(i)
+        o = adinebook.Response(i)
         e = '* {{یادکرد کتاب|نام خانوادگی=فخررحیمی|نام=علیرضا|نام خانوادگی۲=فخررحیمی|نام۲=الهام|دیگران= آرش نادرپور (مقدمه)،  امیر جابری (مقدمه)،  وحید شهبازیان (مقدمه) و  رضا مقدم (مقدمه)|کتاب=آموزش گام به گام پیکربندی مسیریابهای میکروتیک: آمادگی آزمون MTCNA|ناشر=نشرگستر|سال=1391|ماه=خرداد|شابک=978-600-5883-43-5|زبان=fa|تاریخ بازبینی='
         self.assertIn(e, o.ctnt)
 
     def test_ab4(self):
         """authors = 3, translators = 0, otheo = 0, isbn13"""
         i = 'http://www.adinebook.com/gp/product/9649563342/ref=ftr_1/905-6618179-9188955'
-        o = adinebook.AdineBook(i)
+        o = adinebook.Response(i)
         e = '* {{یادکرد کتاب|نام خانوادگی=کریمی|نام=نجمه|نام خانوادگی۲=یزدخواستی|نام۲=فروغ|نام خانوادگی۳=مختاری|نام۳=صفورا|کتاب=11 سپتامبر ... آرماگدون|ناشر=حدیث راه عشق|سال=1386|ماه=شهریور|شابک=978-964-95633-4-3|زبان=fa|تاریخ بازبینی='
         self.assertIn(e, o.ctnt)
 
     def test_ab5(self):
         """Year is interesting here."""
         i = 'http://www.adinebook.com/gp/product/9642656349/'
-        o = adinebook.AdineBook(i)
+        o = adinebook.Response(i)
         e = '* {{یادکرد کتاب|نام خانوادگی=نژاد|نام=یوسف علی یوسف|کتاب=فراهنجاری در مثنوی سرایی|ناشر=اردیبهشت|سال=1388|شابک=978-964-2656-34-9|زبان=fa|تاریخ بازبینی='
         self.assertIn(e, o.ctnt)
 
     def test_ab6(self):
         """Month and year detection."""
         i = 'http://www.adinebook.com/gp/product/9645300363/ref=pd_sim_b_title_4/905-6618179-9188955'
-        o = adinebook.AdineBook(i)
+        o = adinebook.Response(i)
         e = '* {{یادکرد کتاب|نام خانوادگی=مونس|نام=حسین|ترجمه=حمیدرضا شیخی|کتاب=تاریخ و تمدن مغرب|ناشر=سازمان مطالعه و تدوین کتب علوم انسانی دانشگاهها|سال=1392|ماه=شهریور|شابک=978-964-530-036-2|زبان=fa|تاریخ بازبینی='
         self.assertIn(e, o.ctnt)
 
     def test_ab7(self):
         """1 Editor."""
         i = 'http://www.adinebook.com/gp/product/9644593987/ref=pd_pos_b_title_4/905-6618179-9188955'
-        o = adinebook.AdineBook(i)
+        o = adinebook.Response(i)
         e = '* {{یادکرد کتاب|نام خانوادگی=دیماتیو|نام=ام.رابین|نام خانوادگی ویراستار=جباری|نام ویراستار=کریم|ترجمه=محمد کاویانی|دیگران= کیانوش هاشمیان (زيرنظر)|کتاب=روانشناسی سلامت به ضمیمه نگرشی بر منابع اسلامی|ناشر=سازمان مطالعه و تدوین کتب علوم انسانی دانشگاهها|سال=1389|ماه=دی|شابک=978-964-459-398-7|زبان=fa|تاریخ بازبینی='
         self.assertIn(e, o.ctnt)
 
@@ -69,14 +70,14 @@ class GooglebookTest(unittest.TestCase):
 
     def test_gb1(self):
         i = 'http://books.google.com/books?id=pzmt3pcBuGYC&pg=PR11&lpg=PP1&dq=digital+library'
-        o = googlebooks.GoogleBook(i)
+        o = googlebooks.Response(i)
         e = '* {{یادکرد کتاب|نام خانوادگی=Arms|نام=W.Y.|کتاب=Digital Libraries|ناشر=MIT Press|سری=Digital libraries and electronic publishing|سال=2001|شابک=9780262261340|پیوند=http://books.google.com/books?id=pzmt3pcBuGYC&pg=PR11|زبان=en|تاریخ بازبینی='
         self.assertIn(e, o.ctnt)
 
     def test_gb2(self):
         """a book with more than 4 authors (10 authors)"""
         i = 'http://books.google.com/books?id=U46IzqYLZvAC&pg=PT57#v=onepage&q&f=false'
-        o = googlebooks.GoogleBook(i)
+        o = googlebooks.Response(i)
         e1 = '<ref>{{پک|Anderson|DeBolt|Featherstone|Gunther|2010|ک=InterACT with Web Standards: A holistic approach to web design|زبان=en|ص=57}}</ref>'
         e2 = '* {{یادکرد کتاب|نام خانوادگی=Anderson|نام=E.|نام خانوادگی۲=DeBolt|نام۲=V.|نام خانوادگی۳=Featherstone|نام۳=D.|نام خانوادگی۴=Gunther|نام۴=L.|نام خانوادگی۵=Jacobs|نام۵=D.R.|نام خانوادگی۶=Mills|نام۶=C.|نام خانوادگی۷=Schmitt|نام۷=C.|نام خانوادگی۸=Sims|نام۸=G.|نام خانوادگی۹=Walter|نام۹=A.|نام خانوادگی۱۰=Jensen-Inman|نام۱۰=L.|کتاب=InterACT with Web Standards: A holistic approach to web design|ناشر=Pearson Education|سال=2010|شابک=9780132704908|پیوند=http://books.google.com/books?id=U46IzqYLZvAC&pg=PT57|زبان=en|تاریخ بازبینی='
         self.assertIn(e1, o.sfnt)
@@ -85,7 +86,7 @@ class GooglebookTest(unittest.TestCase):
     def test_gb3(self):
         """Non-ascii characters in title"""
         i = 'http://books.google.com/books?id=icMEAAAAQBAJ&pg=PA588&dq=%22a+Delimiter+is%22&hl=en&sa=X&ei=oNKSUrKeDovItAbO_4CoBA&ved=0CC4Q6AEwAA#v=onepage&q=%22a%20Delimiter%20is%22&f=false'
-        o = googlebooks.GoogleBook(i)
+        o = googlebooks.Response(i)
         e1 = '<ref>{{پک|Farrell|ک=Microsoft® Visual C# 2008 Comprehensive: An Introduction to Object-Oriented Programming|زبان=en|ص=588}}</ref>'
         e2 = '* {{یادکرد کتاب|نام خانوادگی=Farrell|نام=J.|کتاب=Microsoft® Visual C# 2008 Comprehensive: An Introduction to Object-Oriented Programming|ناشر=Cengage Learning|شابک=9781111786199|پیوند=http://books.google.com/books?id=icMEAAAAQBAJ&pg=PA588|زبان=en|تاریخ بازبینی='
         self.assertIn(e1, o.sfnt)
@@ -94,7 +95,7 @@ class GooglebookTest(unittest.TestCase):
     def test_gb4(self):
         """Non-ascii characters in author's name."""
         i = 'http://books.google.com/books?id=i8nZjjo_9ikC&pg=PA229&dq=%22legal+translation+is%22&hl=en&sa=X&ei=hEuYUr_mOsnKswb49oDQCA&ved=0CC4Q6AEwAA#v=onepage&q=%22legal%20translation%20is%22&f=false'
-        o = googlebooks.GoogleBook(i)
+        o = googlebooks.Response(i)
         e1 = '<ref>{{پک|Šarčević|1997|ک=New Approach to Legal Translation|زبان=en|ص=229}}</ref>'
         e2 = '* {{یادکرد کتاب|نام خانوادگی=Šarčević|نام=S.|کتاب=New Approach to Legal Translation|ناشر=Kluwer Law International|سال=1997|شابک=9789041104014|پیوند=http://books.google.com/books?id=i8nZjjo_9ikC&pg=PA229|زبان=en|تاریخ بازبینی='
         self.assertIn(e1, o.sfnt)
@@ -105,19 +106,19 @@ class NoormagsTest(unittest.TestCase):
 
     def test_nm1(self):
         i = 'http://www.noormags.com/view/fa/articlepage/5798/102/Text'
-        o = noormags.NoorMag(i)
+        o = noormags.Response(i)
         e = '* {{یادکرد ژورنال|نام خانوادگی=موسوی|نام=زهرا|عنوان=مقرنس در معماری|ژورنال=کتاب ماه هنر|شماره=45|سال=1381|صفحه=102–106|پیوند=http://www.noormags.com/view/fa/articlepage/104040|زبان=fa|تاریخ بازبینی='
         self.assertIn(e, o.ctnt)
 
     def test_nm2(self):
         i = 'http://www.noormags.com/view/fa/ArticlePage/454096'
-        o = noormags.NoorMag(i)
+        o = noormags.Response(i)
         e = '* {{یادکرد ژورنال|عنوان=زندگی نامه علمی دکتر کاووس حسن لی|ژورنال=شعر|شماره=62|سال=1387|صفحه=17–19|پیوند=http://www.noormags.com/view/fa/articlepage/454096|زبان=fa|تاریخ بازبینی='
         self.assertIn(e, o.ctnt)
 
     def test_nm3(self):
         i = 'http://www.noormags.com/view/fa/articlepage/261461'
-        o = noormags.NoorMag(i)
+        o = noormags.Response(i)
         e = '* {{یادکرد ژورنال|نام خانوادگی=ایرانی|نام=هوشنگ|نام خانوادگی۲=آ. ولف|نام۲=|عنوان=لوژیستیک|ژورنال=دانش|شماره=6|سال=1328|صفحه=316–324|پیوند=http://www.noormags.com/view/fa/articlepage/261461|زبان=fa|تاریخ بازبینی='
         self.assertIn(e, o.ctnt)
 
@@ -126,7 +127,7 @@ class NoorlibTest(unittest.TestCase):
 
     def test_nl1(self):
         i = 'http://www.noorlib.ir/View/fa/Book/BookView/Image/18879'
-        o = noorlib.NoorLib(i)
+        o = noorlib.Response(i)
         e = '* {{یادکرد کتاب|نام خانوادگی=مرتضی زبیدی|نام=محمد بن محمد|نام خانوادگی۲=شیری|نام۲=علی|کتاب=تاج العروس من جواهر القاموس|ناشر=دار الفکر|سری=تاج العروس من جواهر القاموس|جلد=10|سال=|پیوند=http://www.noorlib.ir/View/fa/Book/BookView/Image/18879|زبان=عربی|تاریخ بازبینی='
         self.assertIn(e, o.ctnt)
 
@@ -135,18 +136,18 @@ class DoiTest(unittest.TestCase):
 
     def test_di1(self):
         i = 'http://dx.doi.org/10.1038/nrd842'
-        o = doi.Doi(i)
+        o = doi.Response(i)
         e = '* {{یادکرد ژورنال|نام خانوادگی=Atkins|نام=Joshua H.|نام خانوادگی۲=Gershell|نام۲=Leland J.|عنوان=From the analysts couch: Selective anticancer drugs|ژورنال=Nature Reviews Drug Discovery|ناشر=Nature Publishing Group|جلد=1|شماره=7|سال=2002|ماه=Jul|صفحه=491–492|پیوند=http://dx.doi.org/10.1038/nrd842|doi=10.1038/nrd842|زبان=en|تاریخ بازبینی='
         self.assertIn(e, o.ctnt)
 
     def test_di2(self):
         '''DOI with unsafe characters (<>) Warning: this test fials a lot due
-to "HTTPError: HTTP Error 500: Internal Server Error".
-Also be aware that there was an &amp; entity which was manually substitute in
-expected output'''
+        to "HTTPError: HTTP Error 500: Internal Server Error".
+        Also be aware that there was an &amp; entity which was manually substitute in
+        expected output'''
 
         i = '10.1002/(SICI)1097-0010(199604)70:4<422::AID-JSFA514>3.0.CO;2-5'
-        o = doi.Doi(i, pure=True)
+        o = doi.Response(i, pure=True)
         e = '* {{یادکرد ژورنال|نام خانوادگی=Dian|نام=Noor Lida Habi Mat|نام خانوادگی۲=Sudin|نام۲=Nor’aini|نام خانوادگی۳=Yusoff|نام۳=Mohd Suria Affandi|عنوان=Characteristics of Microencapsulated Palm-Based Oil as Affected by Type of Wall Material|ژورنال=Journal of the Science of Food and Agriculture|ناشر=Wiley Blackwell (John Wiley &amp; Sons)|جلد=70|شماره=4|سال=1996|ماه=Apr|صفحه=422–426|پیوند=http://dx.doi.org/10.1002/(SICI)1097-0010(199604)70:4<422::AID-JSFA514>3.0.CO;2-5|doi=10.1002/(SICI)1097-0010(199604)70:4<422::AID-JSFA514>3.0.CO;2-5|زبان=en|تاریخ بازبینی='
         self.assertIn(e, o.ctnt)
 
@@ -156,30 +157,36 @@ class IsbnTest(unittest.TestCase):
     def test_is1(self):
         """not found in adinebook"""
         i = '9780349119168'
-        o = isbn.Isbn(i, pure=True)
+        o = isbn.Response(i, pure=True)
         e = '* {{یادکرد کتاب|نام خانوادگی=Adkins|نام=Roy|کتاب=The war for all the oceans : from Nelson at the Nile to Napoleon at Waterloo|ناشر=Abacus|سال=2007|شابک=9780349119168|زبان=en|تاریخ بازبینی='
         self.assertIn(e, o.ctnt)
 
     def test_is2(self):
         """not found in ottobib"""
         i = '978-964-6736-71-9'
-        o = isbn.Isbn(i, pure=True)
+        o = isbn.Response(i, pure=True)
         e = '* {{یادکرد کتاب|دیگران=بدیل بن علی خاقانی (شاعر)،  جهانگیر منصور (به اهتمام) و  بدیع الزمان فروزانفر (مقدمه)|کتاب=دیوان خاقانی شروانی|ناشر=نگاه|سال=1389|ماه=مرداد|شابک=978-964-6736-71-9|زبان=fa|تاریخ بازبینی='
         self.assertIn(e, o.ctnt)
 
     def test_is3(self):
         """exists in both"""
         i = '964-6736-34-3 '
-        o = isbn.Isbn(i)
+        o = isbn.Response(i)
         e = '* {{یادکرد کتاب|دیگران=سحر معصومی (به اهتمام)|کتاب=راز گل سرخ: نقد و گزیده شعرهای سهراب سپهری|ناشر=نگاه|سال=1386|ماه=بهمن|شابک=964-6736-34-3|زبان=fa|تاریخ بازبینی='
         self.assertIn(e, o.ctnt)
 
     def test_is4(self):
         """unpure isbn10"""
         i = 'choghondar 964-92962-6-3 شلغم'
-        o = isbn.Isbn(i)
+        o = isbn.Response(i)
         e = '* {{یادکرد کتاب|نام خانوادگی=حافظ|نام=شمس الدین محمد|دیگران= رضا نظرزاده (به اهتمام)|کتاب=دیوان کامل حافظ همراه با فالنامه|ناشر=دیوان|سال=1385|ماه=آذر|شابک=964-92962-6-3|زبان=fa|تاریخ بازبینی='
         self.assertIn(e, o.ctnt)
 
+adinebook.requests = dummy_requests.DummyRequests()
+googlebooks.requests = dummy_requests.DummyRequests()
+noormags.requests = dummy_requests.DummyRequests()
+noorlib.requests = dummy_requests.DummyRequests()
+doi.requests = dummy_requests.DummyRequests()
+isbn.requests = dummy_requests.DummyRequests()
 if __name__ == '__main__':
     unittest.main()
