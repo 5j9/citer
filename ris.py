d428ff6994b2d7ea6dad349ac4f0c42a9a21de1d
irdb <irdb@users.noreply.github.com>
2014-10-16 15:45:05 +0330

    catching InvalidNameError exception

--- ris.py
+++ ris.py
@@ -20,7 +20,10 @@ def parse(ris_text):
     if m:
         d['authors'] = []
         for match in m:
-            name = commons.Name(match[1])
+            try:
+                name = commons.Name(match[1])
+            except commons.InvalidNameError:
+                continue
             d['authors'].append(name)
 
     m = re.search('(T1|TI)  - (.*)', ris_text)
