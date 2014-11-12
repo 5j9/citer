4a01cd8dca6c8d5bd1ed9958c201919d7afbc18b
irdb <irdb@users.noreply.github.com>
2014-11-12 12:23:17 +0330

    jslint

--- html_en.py
+++ html_en.py
@@ -122,91 +122,133 @@ template = Template("""<!DOCTYPE html>
 		</div>
 	</div>
 	<script>
-function getCheckedRadio() {
-  radio_group = document.getElementsByName('dateformat')
-  for (var i = 0; i < radio_group.length; i++) {
-    var button = radio_group[i];
-    if (button.checked) {
-      return button;
-    }
-  }
-}
-function Ymd(date) {
-  return date.toISOString().slice(0, 10)
-}
-function BdY(date) {
-  return months[date.getMonth()] + ' ' + date.toISOString().slice(8, 10) + ', ' + date.getFullYear()
-}
-function bdY(date) {
-  return months[date.getMonth()].slice(0, 3) + ' ' + date.toISOString().slice(8, 10) + ', ' + date.getFullYear()
-}
-function dBY(date) {
-  return date.toISOString().slice(8, 10) + ' ' + months[date.getMonth()] + ' ' + date.getFullYear()
-}
-function dbY(date) {
-  return date.toISOString().slice(8, 10) + ' ' + months[date.getMonth()].slice(0, 3) + ' ' + date.getFullYear()
-}
+/*jslint browser: true, regexp: true, white: true */
 var months = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December'
-]
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ];
+
+function getCheckedRadio() {
+	'use strict';
+	var radio_group, i, button;
+	radio_group = document.getElementsByName('dateformat');
+	for (i = 0; i < radio_group.length; i = i + 1) {
+		button = radio_group[i];
+		if (button.checked) {
+			return button;
+		}
+	}
+}
+
+function Ymd(date) {
+	'use strict';
+	return date.toISOString()
+		.slice(0, 10);
+}
+
+function BdY(date) {
+	'use strict';
+	return months[date.getMonth()] + ' ' + date.toISOString()
+		.slice(8, 10) + ', ' + date.getFullYear();
+}
+
+function bdY(date) {
+	'use strict';
+	return months[date.getMonth()].slice(0, 3) + ' ' + date.toISOString()
+		.slice(8, 10) + ', ' + date.getFullYear();
+}
+
+function dBY(date) {
+	'use strict';
+	return date.toISOString()
+		.slice(8, 10) + ' ' + months[date.getMonth()] + ' ' + date.getFullYear();
+}
+
+function dbY(date) {
+	'use strict';
+	return date.toISOString()
+		.slice(8, 10) + ' ' + months[date.getMonth()].slice(0, 3) + ' ' + date.getFullYear();
+}
+
 function changeDates(id) {
-  text1 = document.getElementById('shortened').textContent
-  text2 = document.getElementById('named_ref').textContent
-  dates = text1.match(/date=.*?(?=\}\}|\|)/g)
-  for (var i = 0; i < dates.length; i++) {
-    date = dates[i].slice(5)
-    if (date.contains('-')) {
-      utcdate = date
-    }
-    else {
-      utcdate = date + " UTC"
-    };
-    newdate = eval(id + '(new Date(utcdate))').replace(/^[0]+/g, "").replace(" 0", " ")
-    text1 = text1.replace(RegExp('((?:access)?date=)' + date + '(?=}}|\\|)'), '$1' + newdate)
-    text2 = text2.replace(RegExp('((?:access)?date=)' + date + '(?=}}|\\|)'), '$1' + newdate)
-    document.getElementById('shortened').value = text1
-    document.getElementById('named_ref').value = text2
-  }
+	'use strict';
+	var i, utcdate, text1, text2, dates, date, newdate, formatter;
+	text1 = document.getElementById('shortened')
+		.textContent;
+	text2 = document.getElementById('named_ref')
+		.textContent;
+	dates = text1.match(/date=.*?(?=\}\}|\|)/g);
+	for (i = 0; i < dates.length; i = i + 1) {
+		date = dates[i].slice(5);
+		if (date.contains('-')) {
+			utcdate = date;
+		} else {
+			utcdate = date + " UTC";
+		}
+		formatter = window[id];
+		newdate = formatter(new Date(utcdate))
+			.replace(/^[0]+/g, "")
+			.replace(" 0", " ");
+		text1 = text1.replace(new RegExp('((?:access)?date=)' + date + '(?=}}|\\|)'), '$1' + newdate);
+		text2 = text2.replace(new RegExp('((?:access)?date=)' + date + '(?=}}|\\|)'), '$1' + newdate);
+		document.getElementById('shortened')
+			.value = text1;
+		document.getElementById('named_ref')
+			.value = text2;
+	}
 }
+
 function setCookie(cname, cvalue, exdays) {
-  var d = new Date();
-  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-  var expires = 'expires=' + d.toGMTString();
-  document.cookie = cname + '=' + cvalue + '; ' + expires;
+	'use strict';
+	var expires, d = new Date();
+	d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+	expires = 'expires=' + d.toGMTString();
+	document.cookie = cname + '=' + cvalue + '; ' + expires;
 }
+
 function getCookie(cname) {
-  var name = cname + '=';
-  var ca = document.cookie.split(';');
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i].trim();
-    if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
-  }
-  return '';
+	'use strict';
+	var c, i, ca, name = cname + '=';
+	ca = document.cookie.split(';');
+	for (i = 0; i < ca.length; i = i + 1) {
+		c = ca[i].trim();
+		if (c.indexOf(name) === 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return '';
 }
+
 function checkCookie() {
-  var datefmt = getCookie('datefmt');
-  if (datefmt != '') {
-    document.getElementById(datefmt).checked = true;
-    changeDates(datefmt)
-  }
+	'use strict';
+	var datefmt = getCookie('datefmt');
+	if (datefmt !== '') {
+		document.getElementById(datefmt)
+			.checked = true;
+		changeDates(datefmt);
+	}
 }
+
 function onDateChange() {
-  id = getCheckedRadio() ['id']
-  changeDates(id)
-  setCookie('datefmt', id, 365)
+	'use strict';
+	var id;
+	id = getCheckedRadio()
+		.id;
+	changeDates(id);
+	setCookie('datefmt', id, 365);
 }
-checkCookie()
+checkCookie();
+
 	</script>
 </body>
 
