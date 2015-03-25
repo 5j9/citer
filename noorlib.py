fb4ec0ab9cefb00fb5dc503726460f6d6b5d89cf
irdb <irdb@users.noreply.github.com>
2015-03-25 18:56:44 +0430

    variable rename: noormags_url --> noorlib_url

--- noorlib.py
+++ noorlib.py
@@ -15,20 +15,20 @@ class Response(commons.BaseResponse):
 
     """Create NoorLib response object."""
 
-    def __init__(self, noormags_url, date_format='%Y-%m-%d'):
+    def __init__(self, noorlib_url, date_format='%Y-%m-%d'):
         """Make the dictionary and run self.generate()."""
         self.date_format = date_format
-        self.url = noormags_url
-        self.bibtex = get_bibtex(noormags_url)
+        self.url = noorlib_url
+        self.bibtex = get_bibtex(noorlib_url)
         self.dictionary = bibtex.parse(self.bibtex)
-        #self.ris = get_ris(noormags_url)[1]
+        #self.ris = get_ris(noorlib_url)[1]
         #self.dictionary = ris.parse(self.ris)[1]
         self.generate()
 
 
-def get_bibtex(noormags_url):
+def get_bibtex(noorlib_url):
     """Get bibtex file content from a noormags url. Return as string."""
-    pagetext = requests.get(noormags_url).text
+    pagetext = requests.get(noorlib_url).text
     article_id = re.search(
         'CitationHandler\.ashx\?id=(\d+)',
         pagetext,
@@ -39,11 +39,11 @@ def get_bibtex(noormags_url):
     return bibtex
 
 
-def get_ris(noormags_url):
+def get_ris(noorlib_url):
     # This is copied from noormags module (currently not supported but may
     # be)[1]
     """Get ris file content from a noormags url. Return as string."""
-    pagetext = requests.get(noormags_url).text
+    pagetext = requests.get(noorlib_url).text
     article_id = re.search('RIS&id=(\d+)', pagetext).group(1)
     url = 'http://www.noormags.com/view/CitationHandler.ashx?format=RIS&id=' +\
           article_id
