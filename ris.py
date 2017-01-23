c6d7283afaf67827259002150bbd182bc9bbeea4
5j9 <5j9@users.noreply.github.com>
2017-01-23 17:49:23 +0330

    Use precompiled regexes

--- ris.py
+++ ris.py
@@ -4,76 +4,93 @@
 import re
 from doi import DOI_SEARCH
 
-import commons
+from commons import Name, InvalidNameError
+
+
+TY_SEARCH = re.compile('TY  - (.*)').search
+AU_FINDALL = re.compile('(:?AU|A\d)  - (.*)').findall
+T1_SEARCH = re.compile('(T1|TI)  - (.*)').search
+T3_SEARCH = re.compile('T3  - (.*)').search
+PB_SEARCH = re.compile('PB  - (.*)').search
+JF_SEARCH = re.compile('(JF|JA)  - (.*)').search
+IS_SEARCH = re.compile('IS  - (.*)').search
+VL_SEARCH = re.compile('VL  - (.*)').search
+PY_SEARCH = re.compile('(PY|Y1|DA)  - (\d*)').search
+DA_SEARCH = re.compile('(PY|Y1|DA)  - \d+/(\d*)').search
+SN_SEARCH = re.compile('SN  - (.*)').search
+SP_SEARCH = re.compile('SP  - (.*)').search
+EP_SEARCH = re.compile('EP  - (.*)').search
+UR_SEARCH = re.compile('UR  - (.*)').search
+LA_SEARCH = re.compile('LA  - (.*)').search
 
 
 def parse(ris_text):
     """Parse RIS_text data and return the result as a dictionary."""
     d = {}
     # type: (book, journal, . . . )
-    m = re.search('TY  - (.*)', ris_text)
+    m = TY_SEARCH(ris_text)
     if m:
         d['type'] = m.group(1).strip().lower()
     # author:
-    m = re.findall('(:?AU|A\d)  - (.*)', ris_text)
+    m = AU_FINDALL(ris_text)
     # d['authors'] should not be created unless there are some authors
     if m:
         d['authors'] = []
         for match in m:
             try:
-                name = commons.Name(match[1])
-            except commons.InvalidNameError:
+                name = Name(match[1])
+            except InvalidNameError:
                 continue
             d['authors'].append(name)
 
-    m = re.search('(T1|TI)  - (.*)', ris_text)
+    m = T1_SEARCH(ris_text)
     if m:
         if m.group(2):
             d['title'] = m.group(2).strip()
-    m = re.search('T3  - (.*)', ris_text)
+    m = T3_SEARCH(ris_text)
     if m:
         d['series'] = m.group(1).strip()
-    m = re.search('PB  - (.*)', ris_text)
+    m = PB_SEARCH(ris_text)
     if m:
         d['publisher'] = m.group(1).strip()
-    m = re.search('(JF|JA)  - (.*)', ris_text)
+    m = JF_SEARCH(ris_text)
     if m:
         if m.group(2):
             d['journal'] = m.group(2).strip()
-    m = re.search('IS  - (.*)', ris_text)
+    m = IS_SEARCH(ris_text)
     if m:
         d['issue'] = m.group(1).strip()
-    m = re.search('VL  - (.*)', ris_text)
+    m = VL_SEARCH(ris_text)
     if m:
         d['volume'] = m.group(1).strip()
-    m = re.search('(PY|Y1|DA)  - (\d*)', ris_text)
+    m = PY_SEARCH(ris_text)
     if m:
         if m.group(2):
             d['year'] = m.group(2).strip()
-    m = re.search('(PY|Y1|DA)  - \d+/(\d*)', ris_text)
+    m = DA_SEARCH(ris_text)
     if m:
         if m.group(2):
             d['month'] = m.group(2).strip()
-    m = re.search('SN  - (.*)', ris_text)
+    m = SN_SEARCH(ris_text)
     if m:
         d['isbn'] = m.group(1).strip()
     # DOIs may be in N1 (notes) tag, search for it in any tag
     m = DOI_SEARCH(ris_text)
     if m:
         d['doi'] = m.group(0).strip()
-    m = re.search('SP  - (.*)', ris_text)
+    m = SP_SEARCH(ris_text)
     if m:
         d['startpage'] = m.group(1).strip()
         d['pages'] = d['startpage']
-    m = re.search('EP  - (.*)', ris_text)
+    m = EP_SEARCH(ris_text)
     if m:
         d['endpage'] = m.group(1).strip()
         d['pages'] = d['startpage'] + '–' + d['endpage']
-    m = re.search('UR  - (.*)', ris_text)
+    m = UR_SEARCH(ris_text)
     if m:
         # in IRS, url can be seprated using a ";"
         d['url'] = m.group(1).split(';')[0].strip()
-    m = re.search('LA  - (.*)', ris_text)
+    m = LA_SEARCH(ris_text)
     if m:
         d['language'] = m.group(1).strip()
     return d
