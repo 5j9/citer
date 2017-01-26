625aefae482b5f74db497b9426427a4f65421164
5j9 <5j9@users.noreply.github.com>
2017-01-26 17:30:42 +0330

    Rewrite date processing using regex

--- commons.py
+++ commons.py
@@ -3,76 +3,82 @@
 
 """Common variables, functions, and classes used in string conversions, etc."""
 
-import re
+import regex
 import json
+import calendar
 from datetime import datetime
 from collections import namedtuple
 
 import langid
 from isbnlib import mask as isbn_mask
 from jdatetime import date as jdate
+from datetime import date as gdate
 
 from config import lang
 
 
-FA_MONTHS = (
-    'فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'مرداد', 'شهریور', 'مهر',
-    'آبان', 'آذر', 'دی', 'بهمن', 'اسفند'
-)
+b_TO_NUM = {name: num for num, name in enumerate(calendar.month_abbr) if num}
+B_TO_NUM = {name: num for num, name in enumerate(calendar.month_name) if num}
 
-DOUBLE_DIGIT_SEARCH = re.compile(r'\d\d').search
+# jB_TO_NUM contains entries for both ی and ي
+jB_TO_NUM = {
+    'فروردین': 1,
+    'فروردين': 1,
+    'اردیبهشت': 2,
+    'اردیيهشت': 2,
+    'خرداد': 3,
+    'تیر': 4,
+    'تير': 4,
+    'مرداد': 5,
+    'شهریور': 6,
+    'شهريور': 6,
+    'مهر': 7,
+    'آبان': 8,
+    'آذر': 9,
+    'دی': 10,
+    'دي': 10,
+    'بهمن': 11,
+    'اسفند': 12,
+}
+
+DOUBLE_DIGIT_SEARCH = regex.compile(r'\d\d').search
 
 # Date patterns:
 
 # January|February...
 B = (
+    r'(?<B>'
     r'(?:J(?:anuary|u(?:ne|ly))|February|Ma(?:rch|y)|'
     r'A(?:pril|ugust)|(?:(?:(?:Sept|Nov|Dec)em)|Octo)ber)'
+    r')'
 )
 # فروردین|اردیبهشت|خرداد...
-fa_B = '|'.join(FA_MONTHS).replace('ی', '[یي]')
+jB = '(?<jB>{})'.format(
+    '|'.join([jm for jm in jB_TO_NUM]).replace('ی', '[یي]')
+)
 
 # Month abbreviations:
-b = r'(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec).?'
+b = r'(?<b>Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec).?'
 # Month numbers 0?1-12
-m = r'(?:0?[1-9]|1[012])'
+m = r'(?<m>0?[1-9]|1[012])'
 # Zero padded month numbers 01-12
-zm = r'(?:0[1-9]|1[012])'
+zm = r'(?<m>0[1-9]|1[012])'
 # Day (0?1-31)
-d = r'(?:0?[1-9]|[12][0-9]|3[01])'
+d = r'(?<d>0?[1-9]|[12][0-9]|3[01])(?>st|nd|th)?'
 # Zero-padded day (01-31)
-zd = r'(?:0[1-9]|[12][0-9]|3[01])'
+zd = r'(?<d>0[1-9]|[12][0-9]|3[01])'
 # Gregorian year pattern 1900-2099
-Y = r'(?:19|20)\d\d'
+Y = r'(?<Y>(?:19|20)\d\d)'
 
-# July 3, 2001
-BdY = re.compile(B + ' ' + d + ', ' + Y)
-# Aug 22, 2001
-bdY = re.compile(b + ' ' + d + ', ' + Y)
-# 22 August 2001
-dBY = re.compile(d + ' ' + B + ' ' + Y)
-# ۲۷ مرداد ۱۳۹۳
-fa_dBY = re.compile('(?:\d\d?) (' + fa_B + ') (?:\d\d\d\d)')
-
-# 22 Aug 2001
-dbY = re.compile(d + ' ' + b + ' ' + Y)
-
-# 1900-01-01,2099-12-31
-Ymd_dashed = re.compile(Y + '-' + zm + '-' + zd)
-# 1900/01/01, 2099/12/31, 2099 12 31
-Ymd_slashed = re.compile(Y + '/' + zm + '/' + zd)
-# 19000101, 20991231
-Ymd = re.compile(Y + zm + zd)
-
-ANYDATE_SEARCH = re.compile(
-    '(?:' + B + ' ' + d + ', ' + Y + '|' +
-    b + ' ' + d + ', ' + Y + '|' +
-    d + ' ' + B + ' ' + Y + '|' +
-    d + ' ' + b + ' ' + Y + '|' +
-    Y + '-' + zm + '-' + zd + '|' +
-    Y + '/' + zm + '/' + zd + '|' +
-    '(?P<jd>\d\d?) (?P<jB>' + fa_B + ') (?P<jY>\d\d\d\d)|' +
-    Y + zm + zd + ')'
+# Todo: fstring of 3.6
+ANYDATE_SEARCH = regex.compile(
+    r'(?:'
+    r'(?:{B}|{b}) {d}, {Y}'
+    r'|{d} (?:{B}|{b}) {Y}'
+    r'|{Y}(?<sep>[-/]){zm}\g<sep>{zd}'
+    r'|(?<d>\d\d?) {jB} (?<Y>\d\d\d\d)'
+    r'|{Y}{zm}{zd}'
+    r')'.format(**locals())
 ).search
 
 USER_AGENT_HEADER = {
@@ -221,11 +227,11 @@ def firstname_lastname(fullname, seperator) -> tuple:
        (firstname.islower() and lastname.islower()):
         firstname = firstname.title()
         lastname = lastname.title()
-        lastname = re.sub(
+        lastname = regex.sub(
             'MC(\w)',
             lambda mtch: 'Mc' + mtch.group(1).upper(),
             lastname,
-            flags=re.I
+            flags=regex.I
         )
     if suffix:
         firstname += suffix.title()
@@ -241,7 +247,7 @@ def uninum2en(string) -> str:
     """
     if not string:
         raise ValueError
-    digits = set(re.findall(r'\d', string))
+    digits = set(regex.findall(r'\d', string))
     for digit in digits:
         string = string.replace(digit, str(int(digit)))
     return string
@@ -264,16 +270,7 @@ def ennum2fa(string_or_num) -> str:
     )
 
 
-def jalali_string_to_date(match) -> jdate:
-    """Return the date object for given Jalali string."""
-    return jdate(
-        int(uninum2en(match.group('jY'))),
-        FA_MONTHS.index(match.group('jB').replace('ي', 'ی')) + 1,
-        int(uninum2en(match.group('jd'))),
-    ).togregorian()
-
-
-def finddate(string) -> datetime.date:
+def finddate(string) -> datetime.date or None:
     """Try to find a date in input string and return it as a date object.
 
     If there is no matching date, return None.
@@ -281,65 +278,36 @@ def finddate(string) -> datetime.date:
 
     """
     match = ANYDATE_SEARCH(string)
+    if not match:
+        return None
+    groupdict = match.groupdict()
+    day = int(groupdict['d'])
+    year = int(groupdict['Y'])
+    month = groupdict.get('jB')
     today = datetime.today().date()
-    while match:
-        date_string = match.group(0)
-        try:
-            date = datetime.strptime(date_string, '%B %d, %Y').date()
-            if date <= today:
-                return date
-        except ValueError:
-            pass
-        try:
-            date = datetime.strptime(
-                date_string.replace('.', ''),
-                '%b %d, %Y'
-            ).date()
-            if date <= today:
-                return date
-        except ValueError:
-            pass
-        try:
-            date = datetime.strptime(date_string, '%d %B %Y').date()
-            if date <= today:
-                return date
-        except ValueError:
-            pass
-        try:
-            date = datetime.strptime(
-                date_string.replace('.', ''),
-                '%d %b %Y'
-            ).date()
-            if date <= today:
-                return date
-        except ValueError:
-            pass
-        try:
-            date = datetime.strptime(date_string, '%Y-%m-%d').date()
-            if date <= today:
-                return date
-        except ValueError:
-            pass
-        try:
-            date = datetime.strptime(date_string, '%Y/%m/%d').date()
-            if date <= today:
-                return date
-        except ValueError:
-            pass
-        try:
-            date = jalali_string_to_date(match)
-            if date <= today:
-                return date
-        except ValueError:
-            pass
-        try:
-            date = datetime.strptime(date_string, '%Y%m%d').date()
-            if date <= today:
-                return date
-        except ValueError:
-            pass
-        pos = match.end()
-        match = ANYDATE_SEARCH(string, pos)
+    if month:
+        date = jdate(year, jB_TO_NUM[month], day).togregorian()
+        if date <= today:
+            return date
+        return
+    month = groupdict.get('B')
+    if month:
+        date = gdate(year, B_TO_NUM[month], day)
+        if date <= today:
+            return date
+        return
+    month = groupdict.get('b')
+    if month:
+        date = gdate(year, b_TO_NUM[month], day)
+        if date <= today:
+            return date
+        returnmonth = groupdict.get('b')
+    month = groupdict.get('m')
+    if month:
+        date = gdate(year, int(month), day)
+        if date <= today:
+            return date
+        return
 
 
 def bidi_pop(string) -> str:
