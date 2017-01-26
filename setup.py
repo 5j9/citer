577461b7f6f20f657636bcf8a84e6a4f4751c4d8
5j9 <5j9@users.noreply.github.com>
2017-01-26 17:34:30 +0330

    Add regex>=2017.1.17 to install_requires

--- setup.py
+++ setup.py
@@ -81,6 +81,7 @@ setup(
         'lxml>=3.7.2',
         'flup6>=1.1',
         'jdatetime>=1.8.1',
+        'regex>=2017.1.17',
     ],
 
     # If there are data files included in your packages that need to be
