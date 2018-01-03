4e8eae3e6f462e77c4be1b9f8f0aa84996788a77
5j9 <5j9@users.noreply.github.com>
2018-01-03 21:10:12 +0330

    Simplify ris.parse

--- src/ris.py
+++ src/ris.py
@@ -16,11 +16,6 @@ RIS_FULLMATCH = regex_compile(
             A[U\d]\ {2}-\ (?<author>.++)
             |DA\ {2}-\ \d++/(?<month>\d++).*+
             |EP\ {2}-\ (?<end_page>.++)
-            |T(?>
-                [1I]\ {2}-\ (?<title>.++)
-                |3\ {2}-\ (?<series>.++)
-                |Y\ {2}-\ (?<type>.++)
-            )
             |IS\ {2}-\ (?<issue>.++)
             |J[FA]\ {2}-\ (?<journal>.++)
             |LA\ {2}-\ (?<language>.++)
@@ -31,6 +26,11 @@ RIS_FULLMATCH = regex_compile(
             |S(?>
                 N\ {2}-\ (?<isbn>.++)
                 |P\ {2}-\ (?<start_page>.++)
+            )
+            |T(?>
+                [1I]\ {2}-\ (?<title>.++)
+                |3\ {2}-\ (?<series>.++)
+                |Y\ {2}-\ (?<type>.++)
             )
             |UR\ {2}-\ (?<url>.++)
             |VL\ {2}-\ (?<volume>.++)
@@ -48,12 +48,10 @@ RIS_FULLMATCH = regex_compile(
 def parse(ris_text):
     """Parse RIS_text data and return the result as a dictionary."""
     d = defaultdict(lambda: None)
-    # cite_type: (book, journal, . . . )
     match = RIS_FULLMATCH(ris_text)
-    groupdict_get = match.groupdict().get
-    type_ = groupdict_get('type')
-    if type_:
-        d['cite_type'] = type_.lower()
+    d.update(match.groupdict())
+    # cite_type: (book, journal, . . . )
+    d['cite_type'] = d['type'].lower()
     # author:
     # d['authors'] should not be created unless there are some authors
     authors = match.captures('author')
@@ -66,49 +64,17 @@ def parse(ris_text):
                 continue
             else:
                 d['authors'].append(author)
-    title = groupdict_get('title')
-    if title:
-        d['title'] = title
-    series = groupdict_get('series')
-    if series:
-        d['series'] = series
-    publisher = groupdict_get('publisher')
-    if publisher:
-        d['publisher'] = publisher
-    journal = groupdict_get('journal')
-    if journal:
-        d['journal'] = journal
-    issue = groupdict_get('issue')
-    if issue:
-        d['issue'] = issue
-    volume = groupdict_get('volume')
-    if volume:
-        d['volume'] = volume
-    year = groupdict_get('year')
-    if year:
-        d['year'] = year
-    month = groupdict_get('month')
-    if month:
-        d['month'] = month
-    isbn = groupdict_get('isbn')
-    if isbn:
-        d['isbn'] = isbn
     # DOIs may be in N1 (notes) tag, search for it in any tag
     m = DOI_SEARCH(ris_text)
     if m:
         d['doi'] = m[0]
-    start_page = groupdict_get('start_page')
+    start_page = d['start_page']
     if start_page:
-        end_page = groupdict_get('end_page')
+        end_page = d['end_page']
         if end_page:
             d['page'] = start_page + '–' + end_page
         else:
             d['page'] = start_page
-    url = groupdict_get('url')
-    if url:
-        # in IRS, url can be seprated using a ";"
-        d['url'] = url.partition(';')[0]
-    language = groupdict_get('language')
-    if language:
-        d['language'] = language
+    # in IRS, url can be seprated using a ";"
+    d['url'] = d['url'].partition(';')[0]
     return d
