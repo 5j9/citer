2bcb2fb624fe53edde7d738364d2e36c82e3f841
5j9 <5j9@users.noreply.github.com>
2021-05-28 16:23:57 +0430

    fix(ris): use bibtex
    it's more accurate and AFAIC it does not contain unicode escapes.

--- lib/jstor.py
+++ lib/jstor.py
@@ -1,11 +1,12 @@
 from urllib.parse import urlparse
 
 from lib.commons import request, dict_to_sfn_cit_ref
-from lib.ris import ris_parse
+from lib.bibtex import parse as bibtex_parse
 
 
 def jstor_scr(url: str, date_format: str = '%Y-%m-%d') -> tuple:
-    _, _, id_ = urlparse(url).path.rpartition('/')
-    dictionary = ris_parse(request('https://www.jstor.org/citation/ris/10.2307/' + id_).text)
+    id_ = urlparse(url).path.rpartition('/')[2]
+    bibtex = request('https://www.jstor.org/citation/text/' + id_).text
+    dictionary = bibtex_parse(bibtex)
     dictionary['date_format'] = date_format
     return dict_to_sfn_cit_ref(dictionary)
--- test/jstor_test.py
+++ test/jstor_test.py
@@ -20,9 +20,9 @@ class JSTORTest(TestCase):
     def test_2(self):
         s, c, r = jstor_scr('https://www.jstor.org/stable/resrep26363.7?Search=yes&resultItemClick=true&searchText=google&searchUri=%2Faction%2FdoBasicSearch%3FQuery%3Dgoogle%26acc%3Doff%26wc%3Don%26fc%3Doff%26group%3Dnone%26refreqid%3Dsearch%253A2e627536469ca8786b576957a9797d56&ab_segments=0%2Fbasic_search_gsv2%2Fcontrol&refreqid=fastly-default%3Af90c911269c590baf37330b9d16ae1cd&seq=1#metadata_info_tab_contents')
         self.assertEqual(c[:c.index('| access-date=')], (
-            '* {{cite report | last=Singh | first=Spandana | last2=Blase '
-            '| first2=Margerite | title=Google | publisher=New America '
-            '| series=How Internet Platforms Are Addressing Election and Voter Suppression-Related Misinformation and Disinformation '
+            '* {{cite techreport | last=Singh | first=Spandana | last2=Blase '
+            '| first2=Margerite '
+            '| title=Protecting the Vote: How Internet Platforms Are Addressing Election and Voter Suppression-Related Misinformation and Disinformation '
             '| year=2020 | url=http://www.jstor.org/stable/resrep26363.7 '))
 
 
