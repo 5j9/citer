fad45661bf3a987bde37079f00494a46acf48b2a
5j9 <5j9@users.noreply.github.com>
2022-08-28 04:16:18 +0430

    fix(ketabir)

--- test/ketabir_test.py
+++ test/ketabir_test.py
@@ -1,4 +1,4 @@
-from lib.ketabir import ketabir_scr
+from lib.ketabir import ketabir_scr, isbn2url
 
 
 def test_ab1():
@@ -10,7 +10,7 @@ def test_ab1():
         '| title=101 راه برای اینکه پدر بهتری باشید '
         '| publisher=پیک ادبیات | publication-place=تهران - تهران '
         '| year=1386 | isbn=978-964-8165-81-4 | language=fa}}'
-    ) == ketabir_scr('http://db.ketab.ir/bookview.aspx?bookid=1323394')[1]
+    ) == ketabir_scr('https://ketab.ir/book/27b3444f-1175-4db0-8411-b1719a5d7ed1')[1]
 
 
 def test_ab2():
@@ -19,10 +19,10 @@ def test_ab2():
         '* {{cite book | last=کرسول | first=جان | last2=پلانو‌کلارک '
         '| first2=ویکی '
         '| others=محسن نیازی (مترجم), and عباس زارعی (مترجم) '
-        '| title=روش‌های تحقیق تلفیقی '
-        '| publisher=ثامن‌ الحجج‌(ع) | publication-place=تهران - تهران '
+        '| title=روش\u200cهای تحقیق تلفیقی '
+        '| publisher=ثامن الحجج | publication-place=تهران - تهران '
         '| volume=1 | year=1387 | isbn=978-964-2823-35-2 | language=fa}}'
-    ) == ketabir_scr('http://db.ketab.ir/bookview.aspx?bookid=1369975')[1]
+    ) == ketabir_scr('https://ketab.ir/book/667c900a-69bd-4a1a-a651-1870d2f63a68')[1]
 
 
 def test_ab3():
@@ -37,7 +37,7 @@ def test_ab3():
         ' آمادگی آزمون MTCNA | publisher=نشرگستر '
         '| publication-place=تهران - تهران | year=1391 '
         '| isbn=978-600-5883-43-5 | language=fa}}'
-    ) == ketabir_scr('http://db.ketab.ir/bookview.aspx?bookid=1676357')[1]
+    ) == ketabir_scr('https://ketab.ir/book/f37fad8e-8f0b-4cd9-8875-f5de0e0d86ef')[1]
 
 
 def test_ab4():
@@ -48,7 +48,7 @@ def test_ab4():
         '| title=11 سپتامبر ... آرماگدون | publisher=حدیث راه عشق '
         '| publication-place=اصفهان - اصفهان | year=1386 '
         '| isbn=978-964-95633-4-3 | language=fa}}'
-    ) == ketabir_scr('http://db.ketab.ir/bookview.aspx?bookid=1324978')[1]
+    ) == ketabir_scr('https://ketab.ir/book/13a52229-5e3f-479e-8954-092b65e85923')[1]
 
 
 def test_ab5():
@@ -57,33 +57,35 @@ def test_ab5():
         '* {{cite book | last=یوسف‌نژاد | first=یوسف‌علی '
         '| title=فراهنجاری '
         'در مثنوی‌سرایی: بررسی قالب غزل - مثنوی در ادب فارسی '
-        '| publisher=هنر رسانه اردی‌بهشت '
+        '| publisher=هنر رسانه اردیبهشت '
         '| publication-place=تهران - تهران | year=1388 '
         '| isbn=978-964-2656-34-9 | language=fa}}'
-    ) == ketabir_scr('http://db.ketab.ir/bookview.aspx?bookid=1430801')[1]
+    ) == ketabir_scr('https://ketab.ir/book/a5958832-5c43-460d-bf42-65acb6077e52')[1]
 
 
 def test_ab6():
     """Month and year detection."""
-    assert (
-        '* {{cite book | last=مونس | first=حسین '
-        '| others=حمیدرضا شیخی (مترجم) | title=تاریخ و تمدن مغرب '
-        '| publisher=سازمان‌ مطالعه ‌و تدوین‌ کتب‌ علوم'
-        ' ‌انسانی ‌دانشگاهها (سمت) '
-        '| publication-place=خراسان رضوی - مشهد | volume=1 '
-        '| year=1390 | isbn=978-964-530-036-2 | language=fa}}'
-    ) == ketabir_scr('http://db.ketab.ir/bookview.aspx?bookid=1643445')[1]
+    assert ketabir_scr(
+        'https://ketab.ir/book/cb1989dc-ba09-4df6-aaee-fcdbd25ad322'
+    )[1] == (
+        '* {{cite book | last=مونس | first=حسین | others=حمیدرضا شیخی (مترجم) | '
+        'title=تاریخ و تمدن مغرب | publisher=سمت | publication-place=مشهد - خراسان '
+        'رضوی | volume=1 | year=1390 | isbn=978-964-530-036-2 | language=fa}}'
+    )
 
 
 def test_ab7():
     """1 Editor."""
     assert (
-        '* {{cite book | last=دیماتیو | first=ام.رابین '
-        '| editor-last=جباری | editor-first=کریم '
-        '| others=کیانوش هاشمیان (زيرنظر), and محمد کاویانی (مترجم) '
-        '| title=روانشناسی سلامت به ضمیمه نگرشی بر منابع اسلامی '
-        '| publisher=سازمان‌ مطالعه'
-        ' ‌و تدوین‌ کتب‌ علوم ‌انسانی ‌دانشگاهها (سمت) '
-        '| publication-place=تهران - تهران | volume=1 | year=1379 '
-        '| isbn=964-459-398-7 | language=fa}}'
-    ) == ketabir_scr('https://db.ketab.ir/bookview.aspx?bookid=227129')[1]
+        '* {{cite book | last=دیماتیو | first=ام.رابین | editor-last=جباری | '
+        'editor-first=کریم | others=کیانوش هاشمیان (زيرنظر), and محمد کاویانی (مترجم) '
+        '| title=روانشناسی سلامت به ضمیمه نگرشی بر منابع اسلامی | publisher=سمت | '
+        'publication-place=تهران - تهران | volume=1 | year=1379 | '
+        'isbn=978-964-459-398-7 | language=fa}}'
+    ) == ketabir_scr('https://ketab.ir/book/4cc231f9-35c2-4b60-a714-a0a11135e932')[1]
+
+
+def test_isbn2url():
+    assert isbn2url(
+        '978-964-459-398-7'
+    ) == 'https://ketab.ir/book/4cc231f9-35c2-4b60-a714-a0a11135e932'
