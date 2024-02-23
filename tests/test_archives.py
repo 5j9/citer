702b00f7e56253f5df6f757051c887b6e4f248ec
5j9 <5j9@users.noreply.github.com>
2024-02-23 18:25:37 +0330

    feat(archives): add functions for archive.today

--- tests/test_archives.py
+++ tests/test_archives.py
@@ -1,9 +1,13 @@
-from lib.archives import archive_org_data
+from lib.archives import archive_org_data, archive_today_data
 from lib.commons import data_to_sfn_cit_ref
 
 
-def waybackmachine_scr(*args):
-    return data_to_sfn_cit_ref(archive_org_data(*args))
+def waybackmachine_scr(url):
+    return data_to_sfn_cit_ref(archive_org_data(url))
+
+
+def today_scr(url):
+    return data_to_sfn_cit_ref(archive_today_data(url))
 
 
 def test_live_og_link():
@@ -52,7 +56,7 @@ def test_dead_url():
 
 def test_webless_url():
     """The 'web/ component of the url can be omitted sometimes."""
-    o = waybackmachine_scr(
+    assert waybackmachine_scr(
         'https://web.archive.org/web/20170119050001/'
         'http://www.isna.ir/news/95102918901/'
         '%D8%B1%D9%88%D8%A7%D9%86%DA%86%DB%8C-%D8%AF%D8%B1-'
@@ -63,8 +67,7 @@ def test_webless_url():
         '%D9%85%D9%82%D8%A7%D9%85%D8%A7%D8%AA-'
         '%D8%A7%D9%86%DA%AF%D9%84%DB%8C%D8%B3%DB%8C-'
         '%D8%AF%D8%B1-%D9%85%D9%88%D8%B1%D8%AF'
-    )
-    ct = (
+    )[1][:-12] == (
         '* {{cite web '
         '| title=روانچی: در ارتباط با مواضع نامناسب '
         'اخیر مقامات انگلیسی در مورد ایران گفت‌وگو خواهیم کرد |'
@@ -94,4 +97,13 @@ def test_webless_url():
         '| ref={{sfnref | ایسنا | 2017}} '
         '| access-date='
     )
-    assert ct in o[1]
+
+
+def test_archive_today_data():
+    assert today_scr('https://archive.ph/N3fQ')[1][:-12] == (
+        "* {{cite web | title=Brendan Fraser's Looney Adventure | website=cbsnews.com "
+        '| date=2012-12-11 | '
+        'url=http://www.cbsnews.com/stories/2003/11/12/earlyshow/leisure/celebspot/main583274.shtml '
+        '| archive-url=https://archive.ph/N3fQ | archive-date=2012-12-11 | '
+        'url-status=dead | ref={{sfnref | cbsnews.com | 2012}} | access-date='
+    )
